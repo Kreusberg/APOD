89af5c1852b2273bfe0c05e10aab00adb5f9d651
Kreusberg <kreusberglucas@gmail.com>
2026-03-30 21:43:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Nodes;
+
+string tokenNasa = "xx";
+string tokenBotTelegram = "xxx";
+string chatId = "xxxx";
+
+HttpClient http = new();
+
+string UrlAPOD = $"https://api.nasa.gov/planetary/apod?api_key={tokenNasa}";
+
+string response = await http.GetStringAsync(UrlAPOD);
+
+JsonNode jsonString = JsonNode.Parse(response);
+
+string explanation = jsonString["explanation"]?.ToString();
+string url = jsonString["url"]?.ToString();
+
+var urlTelegram = $"https://api.telegram.org/bot{tokenBotTelegram}/sendPhoto";
+
+FormUrlEncodedContent content = new(new[]
+{
+    new KeyValuePair<string, string>("chat_id", chatId),
+    new KeyValuePair<string, string>("photo", url), 
+    new KeyValuePair<string, string>("caption", explanation), 
+});
+
+await http.PostAsync(urlTelegram, content);
