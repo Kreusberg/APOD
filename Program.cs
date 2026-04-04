c21a7b482ff580f00e6924e2a95be43d93a1043a
Kreusberg <kreusberglucas@gmail.com>
2026-04-04 17:06:58 -0300

    separate logic in Util class

--- Program.cs
+++ Program.cs
@@ -1,55 +1,14 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Nodes;
+using teste;
 
-string tokenNasa = Environment.GetEnvironmentVariable("NASA_TOKEN") ?? "";
-string tokenBotTelegram = Environment.GetEnvironmentVariable("TELEGRAM_TOKEN") ?? "";
-string chatId = Environment.GetEnvironmentVariable("CHAT_ID_TOKEN") ?? "";
-string googleAiToken = Environment.GetEnvironmentVariable("GOOGLE_AI_TOKEN") ?? "";
-
-HttpClient http = new();
-
-string UrlAPOD = $"https://api.nasa.gov/planetary/apod?api_key={tokenNasa}";
-string UrlGoogleAI = $"https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key={googleAiToken}";
-
-string response = await http.GetStringAsync(UrlAPOD);
+string response = await Util.GetApodResponse();
 
 JsonNode jsonString = JsonNode.Parse(response) ?? "";
 
-string explanation = jsonString["explanation"]?.ToString() ?? "";
 string url = jsonString["url"]?.ToString() ?? "";
+string explanation = jsonString["explanation"]?.ToString() ?? "";
 
-var obj = new
-{
-    contents = new[]
-    {
-        new
-        {
-            parts = new[]
-            {
-                new
-                {
-                    text = $"{explanation} - Dado este texto, traduza-o para português (brasileiro). Sua resposta deve conter somente a tradução, sem demais comentários."
-                }
-            }
-        }
-    }
-};
+string explanationPtBr = await Util.GetTranslatedExplanation(explanation);
 
-StringContent googleJson = new(JsonSerializer.Serialize(obj), System.Text.Encoding.UTF8, "application/json");
-
-HttpResponseMessage responseGoogleAI = await http.PostAsync(UrlGoogleAI, googleJson);
-
-JsonNode resposeGoogleJN = JsonNode.Parse(await responseGoogleAI.Content.ReadAsStringAsync()) ?? "";
-
-var explanationPtBr = resposeGoogleJN["candidates"]?[0]?["content"]?["parts"]?[0]?["text"]?.ToString() ?? "";
-
-var urlTelegram = $"https://api.telegram.org/bot{tokenBotTelegram}/sendPhoto";
-
-FormUrlEncodedContent content = new(new[]
-{
-    new KeyValuePair<string, string>("chat_id", chatId),
-    new KeyValuePair<string, string>("photo", url),
-    new KeyValuePair<string, string>("caption", explanationPtBr),
-});
-
-await http.PostAsync(urlTelegram, content);
+await Util.SendTelegramMessage(url, explanationPtBr);
---
+++ Util.cs
@@ -0,0 +1,100 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace teste
+{
+    public class Util
+    {
+        private static readonly HttpClient http = new();
+
+
+        public static async Task<string> GetApodResponse()
+        {
+            string tokenNasa = Environment.GetEnvironmentVariable("NASA_TOKEN") ?? "";
+
+            string UrlAPOD = $"https://api.nasa.gov/planetary/apod?api_key={tokenNasa}";
+            return await http.GetStringAsync(UrlAPOD);
+        }
+
+        public static string CreateGeminiPrompt(string response)
+        {
+            JsonNode jsonString = JsonNode.Parse(response) ?? "";
+
+            string explanation = jsonString["explanation"]?.ToString() ?? "";
+            string url = jsonString["url"]?.ToString() ?? "";
+
+            var obj = new
+            {
+                contents = new[]
+                {
+                    new
+                    {
+                        parts = new[]
+                        {
+                            new
+                            {
+                                text = $"{explanation} - Dado este texto, traduza-o para português (brasileiro). Sua resposta deve conter somente a tradução, sem demais comentários."
+                            }
+                        }
+                    }
+                }
+            };
+
+            return JsonSerializer.Serialize(obj);
+        }
+
+        public static async Task SendTelegramMessage(string url, string explanation)
+        {
+            string tokenBotTelegram = Environment.GetEnvironmentVariable("TELEGRAM_TOKEN") ?? "";
+            string chatId = Environment.GetEnvironmentVariable("CHAT_ID_TOKEN") ?? "";
+
+            var urlTelegram = $"https://api.telegram.org/bot{tokenBotTelegram}/sendPhoto";
+
+            FormUrlEncodedContent content = new(new[]
+            {
+                new KeyValuePair<string, string>("chat_id", chatId),
+                new KeyValuePair<string, string>("photo", url),
+                new KeyValuePair<string, string>("caption", explanation),
+            });
+
+            await http.PostAsync(urlTelegram, content);
+        }
+
+        public static async Task<string> GetTranslatedExplanation(string explanation)
+        {
+            string googleAiToken = Environment.GetEnvironmentVariable("GOOGLE_AI_TOKEN") ?? "";
+            string UrlGoogleAI = $"https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash-lite:generateContent?key={googleAiToken}";
+
+            var obj = new
+            {
+                contents = new[]
+                {
+                    new
+                    {
+                        parts = new[]
+                        {
+                            new
+                            {
+                               text = $"Resuma o texto abaixo em português (Brasil), mantendo o significado original.\n\n" +
+                                $"- Responda apenas com o resumo final.\n" +
+                                $"- NÃO inclua a tradução completa.\n" +
+                                $"- NÃO inclua títulos ou prefixos como 'Resumo:'.\n" +
+                                $"- O resultado deve ter no máximo 1000 caracteres.\n\n" +
+                                $"Texto:\n{explanation}"
+                            }
+                        }
+                    }
+                }
+            };
+
+            StringContent googleJson = new(JsonSerializer.Serialize(obj), System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responseGoogleAI = await http.PostAsync(UrlGoogleAI, googleJson);
+
+            JsonNode resposeGoogleJN = JsonNode.Parse(await responseGoogleAI.Content.ReadAsStringAsync()) ?? "";
+
+            return resposeGoogleJN["candidates"]?[0]?["content"]?["parts"]?[0]?["text"]?.ToString() ?? "";
+
+        }
+    }
+}
